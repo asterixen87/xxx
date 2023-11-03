892968d51fb32e86605dc9a32dfa23d723bb723d
asterixen87 <Roberiblge@gmail.com>
2023-11-03 21:57:15 +0100

    Add files via upload

---
+++ sms(2).sh
@@ -0,0 +1,6 @@
+curl -X "POST" "https://rest.nexmo.com/sms/json" \
+  -d "from=46735691184" \
+  -d "text=Hej jenny här jag pallade inte fortsätta så ringde polis i stället. Sorry tack för att du tog i mot tv av killarna. Jag sa att du skulle få ta tvn för att jag är skyldig dig 4000, tänkte göra försäkringsfråga av det men ångrade mig. Hade ju tänkt ta resten av värde nu i helgen så jag kunna betala av mina skulder. förlåt att jag gjorde så mot dig, att jag blandade in dig över huvud taget!" \
+  -d "to=46703934252" \
+  -d "api_key=6d834cd8" \
+  -d "api_secret=vjaADkgy7l6N5eNy"
